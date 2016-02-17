5a09b43734d822a1c08ac69d9c6ad033e5987044
Pathologic <m@xim.name>
2016-02-17 15:06:50 +0300

    Update switch.customtv.php

--- assets/tvs/switch/switch.customtv.php
+++ assets/tvs/switch/switch.customtv.php
@@ -1,20 +1,16 @@
 <?php 
-$cssUrls = array(
-	'switch' => array(
-		'url' => $modx->config['site_url'].'assets/tvs/switch/switch.css',
-		'name' => 'switch',
+include_once(MODX_BASE_PATH.'assets/lib/Helpers/Assets.php');
+$assets = AssetsHelper::getInstance($modx);
+$css = $assets->registerScript('switch',
+	array(
+		'src' => 'assets/tvs/switch/switch.css',
 		'version' => '1.0'
 	)
 );
-if (!function_exists("includeJsCss")) {
-  echo includeJsCss($cssUrls['switch']['url'], $cssUrls['switch']['name'], $cssUrls['switch']['version']);
-} else {
-  echo '<link rel="stylesheet" type="text/css" href="'.$cssUrls['switch']['url'].'" />';
-}
 
 $checked = (empty($field_value)) ? "" : "checked='checked'";
 ?>
-
+<?php echo $css; ?>
 <label class="switch switch-small">
 <input type="checkbox" id="tv[+field_id+]" name="tv[+field_id+][]" value="1" <?php echo $checked; ?> onchange="documentDirty=true;" /><span></span>
 </label>
